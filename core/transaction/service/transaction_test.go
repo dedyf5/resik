6fd776d186406907758af1ac4f22e0660a01510d
Dedy F. Setyawan <dedyfajars@gmail.com>
2024-03-07 14:55:43 +0700

    add unit test transaction (TestMerchantOmzetGet)

---
+++ core/transaction/service/transaction_test.go
@@ -0,0 +1,118 @@
+// Resik
+// Author: Dedy Fajar Setyawan
+// See: https://github.com/dedyf5/resik
+
+package service
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/dedyf5/resik/config"
+	"github.com/dedyf5/resik/ctx"
+	langCtx "github.com/dedyf5/resik/ctx/lang"
+	configEntity "github.com/dedyf5/resik/entities/config"
+	trxEntity "github.com/dedyf5/resik/entities/transaction"
+	trxParam "github.com/dedyf5/resik/entities/transaction/param"
+	resPkg "github.com/dedyf5/resik/pkg/response"
+	trxRepoMock "github.com/dedyf5/resik/repositories/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/text/language"
+)
+
+func TestMerchantOmzetGet(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	trxRepo := trxRepoMock.NewMockITransaction(ctl)
+	config, ctx := env()
+	trx := New(trxRepo, config)
+
+	t.Run("MerchantOmzetGetTotal-ERROR", func(t *testing.T) {
+		p := trxParam.MerchantOmzetGet{
+			Ctx:        ctx,
+			MerchantID: 1,
+		}
+		errNative := errors.New("failed to get total")
+		statusErr := &resPkg.Status{
+			Code:       http.StatusInternalServerError,
+			CauseError: errNative,
+		}
+		var resUint64 uint64 = 0
+		gomock.InOrder(
+			trxRepo.EXPECT().MerchantOmzetGetTotal(&p).Return(resUint64, statusErr),
+		)
+		res, err := trx.MerchantOmzetGet(&p)
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
+		assert.Equal(t, statusErr.MessageOrDefault(), err.MessageOrDefault())
+		assert.Equal(t, statusErr.CauseError.Error(), err.CauseError.Error())
+	})
+
+	t.Run("MerchantOmzetGetData-ERROR", func(t *testing.T) {
+		p := trxParam.MerchantOmzetGet{
+			Ctx:        ctx,
+			MerchantID: 1,
+		}
+		errNative := errors.New("failed to get data")
+		statusErr := &resPkg.Status{
+			Code:       http.StatusInternalServerError,
+			CauseError: errNative,
+		}
+		var resUint64 uint64 = 1
+		gomock.InOrder(
+			trxRepo.EXPECT().MerchantOmzetGetTotal(&p).Return(resUint64, nil),
+			trxRepo.EXPECT().MerchantOmzetGetData(&p).Return(nil, statusErr),
+		)
+		res, err := trx.MerchantOmzetGet(&p)
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
+		assert.Equal(t, statusErr.MessageOrDefault(), err.MessageOrDefault())
+		assert.Equal(t, statusErr.CauseError.Error(), err.CauseError.Error())
+	})
+
+	t.Run("ALL-SUCCESS", func(t *testing.T) {
+		p := trxParam.MerchantOmzetGet{
+			Ctx:        ctx,
+			MerchantID: 1,
+		}
+		expRes := []trxEntity.MerchantOmzet{}
+		expRes = append(expRes, trxEntity.MerchantOmzet{
+			MerchantID:   1,
+			MerchantName: "Merchant Name",
+			Omzet:        500.75,
+			Period:       "2024-03-07",
+		})
+		var resUint64 uint64 = 1
+		gomock.InOrder(
+			trxRepo.EXPECT().MerchantOmzetGetTotal(&p).Return(resUint64, nil),
+			trxRepo.EXPECT().MerchantOmzetGetData(&p).Return(expRes, nil),
+		)
+		res, err := trx.MerchantOmzetGet(&p)
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, resUint64, res.Total)
+		assert.Equal(t, int(resUint64), len(res.Data))
+		assert.Equal(t, expRes, res.Data)
+	})
+}
+
+func env() (conf config.Config, c *ctx.Ctx) {
+	conf = config.Config{
+		App: configEntity.App{
+			Name:        "Resik",
+			Version:     "0.1",
+			LangDefault: language.English,
+			Host:        "0.0.0.0",
+			Port:        8081,
+		},
+	}
+	c = &ctx.Ctx{
+		Context: context.Background(),
+		Lang:    langCtx.NewLang(language.English, &language.English, ""),
+	}
+	return
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/rs/xid v1.5.0
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.9.0
 	github.com/swaggo/echo-swagger v1.4.1
 	github.com/swaggo/swag v1.16.3
 	go.uber.org/zap v1.26.0
@@ -24,6 +25,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
@@ -33,7 +35,6 @@ require (
 	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/google/subcommands v1.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -60,7 +61,6 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
-	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
